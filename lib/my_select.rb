eed85fd9223dcfdd19e0e7e3351fe7d34edacebf
Katie Blizzard <kblizzard11@gmail.com>
2019-02-09 18:36:20 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
     new_array = []
     while i < collection.length 
       if yield(collection[i]) == true 
-      new_array << collection[i]
+        new_array << collection[i]
       end
       i += 1 
     end
