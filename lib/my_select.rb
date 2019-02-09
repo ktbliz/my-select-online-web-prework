c4cb18c52e2b147f8a29b2c70eb61ded547bb8a4
Katie Blizzard <kblizzard11@gmail.com>
2019-02-09 18:33:27 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(collection)
   if block_given?
     i = 0 
     new_array = []
-    while i < array.length 
-      new_array << yield(array[i])
+    while i < collection.length 
+      new_array << yield(collection[i])
       i += 1 
     end
     return new_array
